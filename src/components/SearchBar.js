ab7f9e365ef131f2faf08a07b79d45ae39127287
Bruno Amorim <bbrunoamorim2311@gmail.com>
2022-10-25 15:07:41 -0300

    adiciona requisito 10

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,7 +1,51 @@
 function SearchBar() {
   return (
     <div>
-      <h1>Hello Search Bar</h1>
+      <section>
+        <label htmlFor="search-input">
+          <input
+            type="text"
+            placeholder="Pesquisa"
+            data-testid="search-input"
+            id="search-input"
+          />
+        </label>
+        <div>
+          <label htmlFor="ingredient-search-radio">
+            Ingrediente
+            <input
+              type="radio"
+              data-testid="ingredient-search-radio"
+              id="ingredient-search-radio"
+              name="search-parameter"
+            />
+          </label>
+          <label htmlFor="name-search-radio">
+            Nome
+            <input
+              type="radio"
+              data-testid="name-search-radio"
+              id="name-search-radio"
+              name="search-parameter"
+            />
+          </label>
+          <label htmlFor="first-letter-search-radio">
+            Primeira letra
+            <input
+              type="radio"
+              data-testid="first-letter-search-radio"
+              id="first-letter-search-radio"
+              name="search-parameter"
+            />
+          </label>
+        </div>
+        <button
+          type="button"
+          data-testid="exec-search-btn"
+        >
+          Pesquisar
+        </button>
+      </section>
     </div>
   );
 }
