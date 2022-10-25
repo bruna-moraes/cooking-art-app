2f49a4f6c97136df40c5661d51376891ef3b8796
Gabriel Coelho <gabrielcoelhodev1995@gmail.com>
2022-10-24 23:32:19 -0300

    requsitos feitos até o 4

---
+++ src/components/Login.js
@@ -0,0 +1,48 @@
+import React, { useContext } from 'react';
+import MyContext from '../context/MyContext';
+
+function Login() {
+  const {
+    email,
+    password,
+    handleChangeEmail,
+    handleChangePassword,
+    submitDisable,
+  } = useContext(MyContext);
+
+  return (
+    <form>
+      <label htmlFor="Email">
+        Email:
+        <input
+          data-testid="email-input"
+          name="Email"
+          value={ email }
+          type="text"
+          onChange={ handleChangeEmail }
+        />
+      </label>
+
+      <label htmlFor="Password">
+        Password:
+        <input
+          data-testid="password-input"
+          name="Password"
+          value={ password }
+          type="Password"
+          onChange={ handleChangePassword }
+        />
+      </label>
+
+      <button
+        data-testid="login-submit-btn"
+        type="button"
+        disabled={ submitDisable }
+      >
+        Login
+      </button>
+    </form>
+  );
+}
+
+export default Login;
