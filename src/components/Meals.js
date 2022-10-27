670839140915f6e40fdae7ec3751fa321a88baf5
Bruna Moraes <brunasthephany.moraes@gmail.com>
2022-10-27 16:30:32 -0300

    feat: Filtrar receitas de comidas e redirecionamento para pagina da receita
    Implementa funcionalidade de filtrar receitas por categoria no componente Meals e adiciona redirect para a pagina RecipeDetails

--- src/components/Meals.js
+++ src/components/Meals.js
@@ -1,30 +1,64 @@
 import { useContext, useEffect } from 'react';
 
+import { Link } from 'react-router-dom';
 import Header from './Header';
 import Footer from './Footer';
 import MyContext from '../context/MyContext';
 import RecipeCard from './RecipeCard';
+import CategoryButton from './CategoryButton';
 
 function Meals() {
-  const { fetchedItems, firstLoadFetch } = useContext(MyContext);
+  const {
+    fetchedItems,
+    firstLoadFetch,
+    categories,
+    loadCategories,
+    filterRecipesByCategory,
+  } = useContext(MyContext);
 
   useEffect(() => {
     firstLoadFetch('Meals');
-  }, [firstLoadFetch]);
+    loadCategories('Meals');
+  }, [firstLoadFetch, loadCategories]);
 
   return (
     <div>
       <Header title="Meals" />
+
+      <div>
+        {
+          categories.map(({ strCategory }) => (
+            <CategoryButton
+              key={ strCategory }
+              name={ strCategory }
+              onClick={ () => filterRecipesByCategory('Meals', strCategory) }
+            />
+          ))
+        }
+
+        <button
+          type="button"
+          data-testid="All-category-filter"
+          onClick={ () => filterRecipesByCategory('Meals', 'All') }
+        >
+          All
+        </button>
+      </div>
+
       {
         fetchedItems.map((meal, index) => (
-          <RecipeCard
+          <Link
             key={ meal.idMeal }
-            recipe={ {
-              name: meal.strMeal,
-              image: meal.strMealThumb,
-            } }
-            index={ index }
-          />
+            to={ `/meals/${meal.idMeal}` }
+          >
+            <RecipeCard
+              recipe={ {
+                name: meal.strMeal,
+                image: meal.strMealThumb,
+              } }
+              index={ index }
+            />
+          </Link>
         ))
       }
       <Footer />
