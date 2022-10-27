dcf15d8d8a32772bd9260917cb4d74cb9cb06dd0
Bruna Moraes <brunasthephany.moraes@gmail.com>
2022-10-27 16:30:48 -0300

    feat: Filtrar receitas de bebidas e redirecionamento para pagina da receita
    Implementa funcionalidade de filtrar receitas por categoria no componente Drinks e adiciona redirect para a pagina RecipeDetails

--- src/components/Drinks.js
+++ src/components/Drinks.js
@@ -1,30 +1,65 @@
 import { useContext, useEffect } from 'react';
+import { Link } from 'react-router-dom';
 
 import Footer from './Footer';
 import Header from './Header';
 import MyContext from '../context/MyContext';
 import RecipeCard from './RecipeCard';
+import CategoryButton from './CategoryButton';
 
 function Drinks() {
-  const { fetchedItems, firstLoadFetch } = useContext(MyContext);
+  const {
+    fetchedItems,
+    firstLoadFetch,
+    categories,
+    loadCategories,
+    filterRecipesByCategory,
+  } = useContext(MyContext);
 
   useEffect(() => {
     firstLoadFetch('Drinks');
-  }, [firstLoadFetch]);
+    loadCategories('Drinks');
+  }, [firstLoadFetch, loadCategories]);
 
   return (
     <div>
       <Header title="Drinks" />
+
+      <div>
+        {
+          categories.map(({ strCategory }) => (
+            <CategoryButton
+              key={ strCategory }
+              name={ strCategory }
+              onClick={ () => filterRecipesByCategory('Drinks', strCategory) }
+            />
+          ))
+        }
+
+        <button
+          type="button"
+          data-testid="All-category-filter"
+          onClick={ () => filterRecipesByCategory('Drinks', 'All') }
+        >
+          All
+        </button>
+      </div>
+
       {
         fetchedItems.map((drink, index) => (
-          <RecipeCard
+          <Link
             key={ drink.idDrink }
-            recipe={ {
-              name: drink.strDrink,
-              image: drink.strDrinkThumb,
-            } }
-            index={ index }
-          />
+            to={ `/drinks/${drink.idDrink}` }
+          >
+            <RecipeCard
+              key={ drink.idDrink }
+              recipe={ {
+                name: drink.strDrink,
+                image: drink.strDrinkThumb,
+              } }
+              index={ index }
+            />
+          </Link>
         ))
       }
       <Footer />
